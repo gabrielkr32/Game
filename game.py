6297e4f3a2ad7142c86dd88697f1cf4f036fd430
Gabriel Kruger <gabrielkr32@gmail.com>
2023-11-13 23:29:32 -0300

    Add map´s

--- game.py
+++ game.py
@@ -17,6 +17,22 @@ class Game:
     def update(self):
         self.map.update()
         self.hero.update()
+        if self.hero.position_x < 0:
+            self.hero.position_x = 160
+            self.map.x -= 1
+
+        if self.hero.position_x > 160:
+            self.hero.position_x = 0
+            self.map.x += 1
+
+        if self.hero.position_y < 0:
+            self.hero.position_y = 120
+            self.map.y -= 1
+
+
+        if self.hero.position_y > 120:
+            self.hero.position_y = 0
+            self.map.y += 1
 
     def draw(self):
         pyxel.cls(0)
